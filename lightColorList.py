7c094508878e4a3c7ba243edacf042ecd5d7a7d3
Ian Massingham <ianmas@amazon.co.uk>
2016-07-18 10:23:23 +0000

    First commit

---
+++ lightColorList.py
@@ -0,0 +1,29 @@
+import time
+
+from sense_hat import SenseHat
+sense = SenseHat()
+
+off = [[0,0,0]] * 64
+red = [[255,0,0]] * 64
+green = [[0,255,0]] * 64
+blue = [[0,0,255]] * 64
+yellow = [[255,255,0]] * 64
+orange = [[255,140,0]] * 64
+purple = [[255,0,255]] * 64
+on = [[255,255,255]] * 64
+
+senseColors = { 'off' : off }
+senseColors['r'] = red
+senseColors['g'] = green
+senseColors['b'] = blue
+senseColors['y'] = yellow
+senseColors['o'] = orange
+senseColors['p'] = purple
+senseColors['on'] = on
+
+def lightColorList ( colorList ):
+   for color in colorList:
+      sense.set_pixels(senseColors[color])
+      time.sleep(1)
+   sense.set_pixels(senseColors['off'])
+   return
---
+++ runShadowEcho.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python ThingShadowEcho.py -e a63r8gv1dbslu.iot.eu-west-1.amazonaws.com -r ../keys/root-CA.crt -c ../keys/e0f88ae69a-certificate.pem.crt -k ../keys/e0f88ae69a-private.pem.key
